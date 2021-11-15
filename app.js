e53c75e380bc78d074afb0a3a3af95cdb1b418fe
od2001 <91567960+od2001@users.noreply.github.com>
2021-11-15 21:01:48 +0000

    Q4 Done

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 //creating app
 const app = express();
+//send an HTTP response when receiving HTTP GET /
+//send the index.html when receiving HTTP GET /
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
