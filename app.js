a6da55a10258b749d12b0fdd7ee4b03194bb14b4
od2001 <91567960+od2001@users.noreply.github.com>
2021-11-24 23:24:46 +0000

    Works but api's needed

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 
 var mysql = require("mysql");
 
+// using JSON and URL Encoded middleware
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 var con = mysql.createConnection({
   host: "sql4.freemysqlhosting.net",
   user: "sql4453466",
@@ -19,10 +25,16 @@ con.connect(function (err) {
 });
 //send an HTTP response when receiving HTTP GET /
 //handling static HTML and EJS templates
+//handling static HTML and EJS templates
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index"); //no need for ejs extension
+});
+
+//send the index.html when receiving HTTP GET /
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
 });
 
 //route for contacts
@@ -35,22 +47,13 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", (req, res) => {
-  res.render("index");
+app.get("/catalog", (req, res) => {
+  res.render("catalog");
 });
 
-// app.post("/auth", function (req, res) {
-//   var username = app.body.username;
-//   var password = app.body.password;
-//   con.query(
-//     "INSERT INTO account (username, password) VALUES (?,?)",
-//     [username, password],
-//     function (err, result) {
-//       if (err) throw err;
-//       console.log("1 record inserted");
-//     }
-//   );
-// });
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
@@ -58,9 +61,6 @@ const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
 });
 
-//handling static HTML and EJS templates
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index"); //no need for ejs extension
-});
+//pass requests to the router middleware
+const router = require("./routes/apis.js");
+app.use(router);
